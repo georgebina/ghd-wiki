76b029a03f363c4e46ee882daf9af6abb0167833
George Bina <george@oxygenxml.com>
2016-08-18 11:31:03 +0300

    Remove also downloaded archives.

--- clean.sh
+++ clean.sh
@@ -3,10 +3,11 @@ echo "====================================="
 echo "remove downloaded DITA-OT"
 echo "====================================="
 rm -Rf dita-ot-2.2.3
-rm dita-ot-2.2.3.zip
 
 echo "====================================="
 echo "remove downloaded Saxon9"
 echo "====================================="
 rm -Rf saxon9
 rm SaxonHE9-7-0-7J.zip
+
+rm *.zip
