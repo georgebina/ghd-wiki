20817b8292a9b70b150ed4c74df6c9fe890fedf3
oXygen XML Deployer <support@oxygenxml.com>
2016-08-31 09:44:00 +0000

    rebuild pages at 0e72d70

--- oxygen-webhelp/search/htmlFileList.js
+++ oxygen-webhelp/search/htmlFileList.js
@@ -2,7 +2,8 @@
 fl = new Array();
 fl["0"]= "simple/index.html";
 fl["1"]= "simple/test.html";
-fl["2"]= "wiki/index.html";
-fl["3"]= "wiki/test.html";
-fl["4"]= "wiki/testMarkDown.html";
+fl["2"]= "wiki/article.html";
+fl["3"]= "wiki/index.html";
+fl["4"]= "wiki/test.html";
+fl["5"]= "wiki/testMarkDown.html";
 var doStem = false;searchLoaded = true;
