c5ea9ab7b2aa9914e4aad160f3fcaeb73d041a32
oXygen XML Deployer <support@oxygenxml.com>
2016-08-31 14:19:52 +0000

    rebuild pages at d9c0327

---
+++ oxygen-webhelp/search/index-1.js
@@ -0,0 +1,61 @@
+// Auto generated list of analyzer stop words that must be ignored by search.
+stopWords = new Array();
+stopWords[0]= "but";
+stopWords[1]= "be";
+stopWords[2]= "with";
+stopWords[3]= "such";
+stopWords[4]= "then";
+stopWords[5]= "for";
+stopWords[6]= "no";
+stopWords[7]= "will";
+stopWords[8]= "not";
+stopWords[9]= "are";
+stopWords[10]= "and";
+stopWords[11]= "their";
+stopWords[12]= "if";
+stopWords[13]= "this";
+stopWords[14]= "on";
+stopWords[15]= "into";
+stopWords[16]= "a";
+stopWords[17]= "or";
+stopWords[18]= "there";
+stopWords[19]= "in";
+stopWords[20]= "that";
+stopWords[21]= "they";
+stopWords[22]= "was";
+stopWords[23]= "is";
+stopWords[24]= "it";
+stopWords[25]= "an";
+stopWords[26]= "the";
+stopWords[27]= "as";
+stopWords[28]= "at";
+stopWords[29]= "these";
+stopWords[30]= "by";
+stopWords[31]= "to";
+stopWords[32]= "of";
+
+var indexerLanguage="en";
+
+// Auto generated index for searching.
+w["article"]="2*30";
+w["author"]="0*0,2*0,3*1,4*0,5*0";
+w["automatically"]="0*0,3*26";
+w["branch"]="0*1,3*27";
+w["commit"]="0*0,3*26";
+w["commited"]="0*0,3*0";
+w["conflict"]="1*0,4*26";
+w["content"]="0*3,1*30,3*55,4*30";
+w["contribution"]="0*0,3*0";
+w["converted"]="0*0,3*26";
+w["create"]="0*0,1*31,3*0,4*30";
+w["dita"]="0*32,3*82";
+w["each"]="0*0,3*26";
+w["edit"]="0*0,1*0,2*10,3*10,4*10,5*10";
+w["editing"]="0*0,3*0";
+w["file"]="0*0,1*0";
+w["folder"]="0*0,3*26";
+w["from"]="0*0,3*26";
+w["generated"]="2*0,3*0,4*0,5*0";
+w["george"]="2*26";
+w["gh"]="0*0,3*0";
+
