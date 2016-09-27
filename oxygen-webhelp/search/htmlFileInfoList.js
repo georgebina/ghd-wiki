795527f08f326ec15943cd8d9306334ed1efd963
oXygen XML Deployer <support@oxygenxml.com>
2016-09-27 14:42:16 +0000

    rebuild pages at b8b6c09

--- oxygen-webhelp/search/htmlFileInfoList.js
+++ oxygen-webhelp/search/htmlFileInfoList.js
@@ -1,7 +1,7 @@
 fil = new Array();
-fil["0"]= "simple/index.html@@@DITA 2 Wiki George@@@...";
+fil["0"]= "simple/index.html@@@DITA 2 Wiki@@@...";
 fil["1"]= "simple/test.html@@@How to create content@@@...";
 fil["2"]= "wiki/article.html@@@Test new article@@@George…...";
-fil["3"]= "wiki/index.html@@@DITA 2 Wiki George@@@This project publishes DITA content from the  wiki  folder automatically using an XSLT script that is triggered on each commit on the master branch through Travis. The DITA content is converted to...";
+fil["3"]= "wiki/index.html@@@DITA 2 Wiki@@@This project publishes DITA content from the  wiki  folder automatically using an XSLT script that is triggered on each commit on the master branch through Travis. The DITA content is converted to...";
 fil["4"]= "wiki/test.html@@@How to create content@@@This is a smaller sample. Radu This is a test Updated. Conflict?...";
 fil["5"]= "wiki/testMarkDown.html@@@TEST MD@@@Test MarkDown…...";
