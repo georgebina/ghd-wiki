7a6ac06d8a574e9f68b75f1e0e32decc0a64a265
oXygen XML Deployer <support@oxygenxml.com>
2016-09-29 12:51:51 +0000

    rebuild pages at d8e11f2

---
+++ oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,9 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "simple/index.html";
+fl["1"]= "simple/test.html";
+fl["2"]= "wiki/article.html";
+fl["3"]= "wiki/index.html";
+fl["4"]= "wiki/test.html";
+fl["5"]= "wiki/testMarkDown.html";
+var doStem = false;searchLoaded = true;
