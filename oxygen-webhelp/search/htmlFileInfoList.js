5d2b2564110d6a8a8e7ba80b33938c2e7bc5ebed
oXygen XML Deployer <support@oxygenxml.com>
2016-10-16 12:48:20 +0000

    rebuild pages at 060e34e

--- oxygen-webhelp/search/htmlFileInfoList.js
+++ oxygen-webhelp/search/htmlFileInfoList.js
@@ -1,7 +1,5 @@
 fil = new Array();
 fil["0"]= "simple/index.html@@@DITA 2 Wiki@@@...";
-fil["1"]= "simple/test.html@@@How to create content@@@...";
-fil["2"]= "wiki/article.html@@@Test new article@@@George…...";
-fil["3"]= "wiki/index.html@@@DITA 2 Wiki@@@This project publishes DITA content from the  wiki  folder automatically using an XSLT script that is triggered on each commit on the master branch through Travis. The DITA content is converted to...";
-fil["4"]= "wiki/test.html@@@How to create content@@@This is a smaller sample. Radu George This is a test Updated. Conflict?...";
-fil["5"]= "wiki/testMarkDown.html@@@TEST MD@@@Test MarkDown…...";
+fil["1"]= "simple/topic.html@@@Lightweight DITA@@@...";
+fil["2"]= "wiki/index.html@@@DITA 2 Wiki@@@This project shows how we can combine DITA, Lightweight DITA and Markdown plus a few services like GitHub, GitHub Pages, Travis, oXygen XML Author, the DITA Open Toolkit with a few plugins for...";
+fil["3"]= "wiki/topic.html@@@Lightweight DITA@@@Lightweight DITA can also be used and you can take advantage of the user interface that controls the authoring experience by providing placeholders, hints, inline actions and inline form controls to edit attributes - all combined to provide a simple user interface suitable for any contributors...";
--- oxygen-webhelp/search/htmlFileList.js
+++ oxygen-webhelp/search/htmlFileList.js
@@ -1,9 +1,7 @@
 //List of files which are indexed.
 fl = new Array();
 fl["0"]= "simple/index.html";
-fl["1"]= "simple/test.html";
-fl["2"]= "wiki/article.html";
-fl["3"]= "wiki/index.html";
-fl["4"]= "wiki/test.html";
-fl["5"]= "wiki/testMarkDown.html";
+fl["1"]= "simple/topic.html";
+fl["2"]= "wiki/index.html";
+fl["3"]= "wiki/topic.html";
 var doStem = false;searchLoaded = true;
