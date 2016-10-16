154c51a595027698042545a103047d1593f527cd
oXygen XML Deployer <support@oxygenxml.com>
2016-10-16 17:35:21 +0000

    rebuild pages at 2e7121b

--- oxygen-webhelp/search/htmlFileInfoList.js
+++ oxygen-webhelp/search/htmlFileInfoList.js
@@ -3,4 +3,4 @@ fil["0"]= "simple/index.html@@@GitHub DITA Wiki@@@...";
 fil["1"]= "simple/topic.html@@@Lightweight DITA@@@...";
 fil["2"]= "wiki/index.html@@@GitHub DITA Wiki@@@This project shows how we can combine DITA, Lightweight DITA and Markdown plus a few services like GitHub, GitHub Pages, Travis, oXygen XML Author, the DITA Open Toolkit with a few plugins for...";
 fil["3"]= "wiki/markdown.html@@@Markdown@@@Markdown can also be used to create pages, so people with Markdown only knowledge will still be able to create a page and use Markdown as the source for that. For now the editing of Markdown content...";
-fil["4"]= "wiki/topic.html@@@Lightweight DITA@@@Lightweight DITA can also be used and you can take advantage of the user interface that controls the authoring experience by providing placeholders, hints, inline actions and inline form controls to edit attributes - all combined to provide a simple user interface suitable for any contributors...";
+fil["4"]= "wiki/topic.html@@@Lightweight DITA@@@Lightweight DITA can be used for ghd-wiki taking advantage of controlled authoring experience by providing placeholders, hints, inline actions and inline form controls...";
