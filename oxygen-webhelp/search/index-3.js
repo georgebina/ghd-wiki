315562e7a8eb3fb3529193d32bd2a2557b6ea40e
oXygen XML Deployer <support@oxygenxml.com>
2016-10-27 20:36:19 +0000

    rebuild pages at 2e8af3c

--- oxygen-webhelp/search/index-3.js
+++ oxygen-webhelp/search/index-3.js
@@ -1,7 +1,5 @@
 
 // Auto generated index for searching.
-w["provide"]="1*0,4*0";
-w["provides"]="1*0,4*0";
 w["providing"]="1*0,3*0,4*26";
 w["publish"]="0*0,2*0";
 w["publishing"]="0*0,2*0";
@@ -20,9 +18,10 @@ w["so"]="3*26";
 w["some"]="1*0,4*0";
 w["soon"]="0*0,2*0";
 w["source"]="3*26";
+w["spec"]="4*0";
 w["special"]="1*0,4*0";
 w["specific"]="3*0";
-w["specified"]="1*0,4*0";
+w["specified"]="1*0";
 w["standard"]="0*0,2*0";
 w["still"]="3*26";
 w["suitable"]="1*0,4*0";
@@ -44,6 +43,7 @@ w["used"]="1*0,3*26,4*26";
 w["user"]="1*1,4*1";
 w["using"]="0*1,2*1";
 w["values"]="1*0,4*0";
+w["vegas"]="1*30,2*0,4*30";
 w["very"]="1*1,4*1";
 w["we"]="0*2,2*28,3*0";
 w["web"]="0*1,1*0,2*1,3*0,4*0";
