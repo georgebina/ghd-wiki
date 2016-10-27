b41594825b9730ef77b0ec830ea34083453ec86e
oXygen XML Deployer <support@oxygenxml.com>
2016-10-27 17:55:35 +0000

    rebuild pages at 5e7a463

--- oxygen-webhelp/search/index-2.js
+++ oxygen-webhelp/search/index-2.js
@@ -1,5 +1,6 @@
 
 // Auto generated index for searching.
+w["form"]="1*1,4*27";
 w["format"]="0*0,2*0";
 w["formats"]="0*0,2*0";
 w["from"]="1*0,4*0";
@@ -13,7 +14,6 @@ w["github"]="0*33,2*83,3*0";
 w["great"]="1*0,4*0";
 w["handling"]="0*0,2*0";
 w["having"]="1*0,4*0";
-w["he"]="4*0";
 w["help"]="1*0,4*0";
 w["highlight"]="3*0";
 w["hints"]="1*1,4*27";
@@ -24,7 +24,6 @@ w["inline"]="1*2,4*53";
 w["interface"]="1*1,4*1";
 w["just"]="0*0,2*0";
 w["knowledge"]="3*26";
-w["lavacon"]="1*30,2*0,4*30";
 w["less"]="1*0,4*0";
 w["lightweight"]="0*0,1*32,2*27,4*57";
 w["like"]="0*1,2*27";
@@ -60,4 +59,5 @@ w["preview"]="3*0";
 w["project"]="0*0,1*0,2*26,4*26";
 w["provide"]="1*0,4*0";
 w["provides"]="1*0,4*0";
+w["providing"]="1*0,3*0,4*26";
 
