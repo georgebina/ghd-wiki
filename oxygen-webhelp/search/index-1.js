bf90f76f39939e33f2bf87bcf7430b255c0e4586
oXygen XML Deployer <support@oxygenxml.com>
2016-11-10 10:10:28 +0000

    rebuild pages at e23645c

--- oxygen-webhelp/search/index-1.js
+++ oxygen-webhelp/search/index-1.js
@@ -38,9 +38,8 @@ var indexerLanguage="en";
 
 // Auto generated index for searching.
 w["able"]="3*26";
-w["ac"]="4*0";
 w["accepted"]="0*0,2*0";
-w["actions"]="1*1,3*0,4*26";
+w["actions"]="1*1,3*0,4*27";
 w["actual"]="1*0,4*0";
 w["advantage"]="1*1,4*27";
 w["all"]="1*0,4*0";
@@ -89,6 +88,7 @@ w["edit"]="0*0,1*1,2*10,3*10,4*11";
 w["editing"]="3*27";
 w["editor"]="3*0";
 w["editors"]="3*0";
+w["eholders"]="4*0";
 w["especially"]="1*0,4*0";
 w["etc"]="0*0,2*0";
 w["example"]="1*0,4*0";
