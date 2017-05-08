5e73d6808e0cae0f174a2fbb322efbe40abbaa51
oXygen XML Deployer <support@oxygenxml.com>
2017-05-08 00:04:21 +0000

    rebuild pages at 5b63467

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20170507113143", "oxygen-webhelp/search/index-1.js?uniqueId=20170507113143", "oxygen-webhelp/search/index-2.js?uniqueId=20170507113143", "oxygen-webhelp/search/index-3.js?uniqueId=20170507113143"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20170508120411", "oxygen-webhelp/search/index-1.js?uniqueId=20170508120411", "oxygen-webhelp/search/index-2.js?uniqueId=20170508120411", "oxygen-webhelp/search/index-3.js?uniqueId=20170508120411"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
