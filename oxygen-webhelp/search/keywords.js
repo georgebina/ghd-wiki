fd6cecae8b48336b117682ff1a43b383b4860a12
oXygen XML Deployer <support@oxygenxml.com>
2017-05-23 15:12:11 +0000

    rebuild pages at d653bbb

--- oxygen-webhelp/search/keywords.js
+++ oxygen-webhelp/search/keywords.js
@@ -1,5 +1,5 @@
-var keywords=[{w:"GitHub",p:["p0"]},{w:"DITA-based",p:["p0"]},{w:"Wiki",p:["p0"]},{w:"LW-DITA",p:["p1"]},{w:"Markdown",p:["p2"]}];
+var keywords=[{w:"GitHub",p:["p0"]},{w:"DITA-based",p:["p0"]},{w:"Wiki",p:["p0"]},{w:"LW-DITA",p:["p1"]},{w:"Markdown",p:["p2"]},{w:"GEORGE",p:["p2"]}];
 var ph={};
-ph["p2"]=[4];
+ph["p2"]=[4, 5];
 ph["p1"]=[3];
 ph["p0"]=[0, 1, 2];
