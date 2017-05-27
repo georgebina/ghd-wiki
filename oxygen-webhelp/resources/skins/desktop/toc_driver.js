a5a9e85002ef72f3ef536697c78b68b6e030e918
oXygen XML Deployer <support@oxygenxml.com>
2017-05-27 10:54:12 +0000

    rebuild pages at ddd6a8c

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20170524101307", "oxygen-webhelp/search/index-1.js?uniqueId=20170524101307", "oxygen-webhelp/search/index-2.js?uniqueId=20170524101307", "oxygen-webhelp/search/index-3.js?uniqueId=20170524101307"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20170527105402", "oxygen-webhelp/search/index-1.js?uniqueId=20170527105402", "oxygen-webhelp/search/index-2.js?uniqueId=20170527105402", "oxygen-webhelp/search/index-3.js?uniqueId=20170527105402"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
--- oxygen-webhelp/search/keywords.js
+++ oxygen-webhelp/search/keywords.js
@@ -1,5 +1,5 @@
-var keywords=[{w:"GitHub",p:["p0"]},{w:"DITA-based",p:["p0"]},{w:"Wiki",p:["p0"]},{w:"LW-DITA",p:["p1"]},{w:"at",p:["p1"]},{w:"Lavacon",p:["p1"]},{w:"Markdown",p:["p2"]}];
+var keywords=[{w:"GitHub",p:["p0"]},{w:"DITA-based",p:["p0"]},{w:"Wiki",p:["p0"]},{w:"LW-DITA",p:["p1"]},{w:"Markdown",p:["p2"]}];
 var ph={};
-ph["p2"]=[6];
-ph["p1"]=[3, 4, 5];
+ph["p2"]=[4];
+ph["p1"]=[3];
 ph["p0"]=[0, 1, 2];
