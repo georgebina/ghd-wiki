410fcd3889ac21a17f4137af77598d01a99a920c
oXygen XML Deployer <support@oxygenxml.com>
2017-07-11 05:07:16 +0000

    rebuild pages at 2d96d20

--- oxygen-webhelp/search/htmlFileInfoList.js
+++
@@ -1,9 +0,0 @@
-fil = new Array();
-fil["0"]= "simple/index.html@@@GitHub DITA-based Wiki@@@...";
-fil["1"]= "simple/sample.html@@@@@@...";
-fil["2"]= "simple/topic.html@@@LW-DITA@@@...";
-fil["3"]= "wiki/index.html@@@GitHub DITA-based Wiki@@@The ghd-wiki project shows how we can combine DITA, Lightweight DITA and Markdown plus a few services like GitHub, GitHub Pages, Travis, oXygen XML Author, the DITA Open Toolkit with a few plugins for...";
-fil["4"]= "wiki/markdown.html@@@Markdown@@@Markdown can also be used to create pages, so people with Markdown only knowledge will still be able to create a page and use Markdown as the source for that. For now the editing of Markdown content...";
-fil["5"]= "wiki/sample.html@@@***@@@...";
-fil["6"]= "wiki/topic.html@@@LW-DITA@@@Lightweight DITA can be used in the ghd-wiki project, thus you can take advantage of controlled authoring experience by providing placeholders, hints, inline actions and inline form controls...";
-var doStem = false;searchLoaded = true;
--- oxygen-webhelp/search/keywords.js
+++
@@ -1,6 +0,0 @@
-var keywords=[{w:"GitHub",p:["p0"]},{w:"DITA-based",p:["p0"]},{w:"Wiki",p:["p0"]},{w:"LW-DITA",p:["p1"]},{w:"Markdown",p:["p2"]},{w:"***",p:["p3"]}];
-var ph={};
-ph["p3"]=[5];
-ph["p2"]=[4];
-ph["p1"]=[3];
-ph["p0"]=[0, 1, 2];
