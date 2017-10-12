3ff16c8af40c3f7f8660544882a532f1e5594007
oXygen XML Deployer <support@oxygenxml.com>
2017-10-12 06:52:43 +0000

    rebuild pages at 6cbd6e7

---
+++ oxygen-webhelp/search/keywords.js
@@ -0,0 +1,5 @@
+var keywords=[{w:"GitHub",p:["p0"]},{w:"DITA-based",p:["p0"]},{w:"Wiki",p:["p0"]},{w:"LW-DITA",p:["p1"]},{w:"Markdown",p:["p2"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3];
+ph["p2"]=[4];
