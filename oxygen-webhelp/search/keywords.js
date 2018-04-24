75da876fa964949135540f3e3ded0240a230df54
oXygen XML Deployer <support@oxygenxml.com>
2018-04-24 20:20:16 +0000

    rebuild pages at cdafc02

--- oxygen-webhelp/search/keywords.js
+++
@@ -1,5 +0,0 @@
-var keywords=[{w:"GitHub",p:["p0"]},{w:"DITA-based",p:["p0"]},{w:"Wiki",p:["p0"]},{w:"LW-DITA",p:["p1"]},{w:"GEORGE",p:["p1"]},{w:"Markdown",p:["p2"]}];
-var ph={};
-ph["p0"]=[0, 1, 2];
-ph["p1"]=[3, 4];
-ph["p2"]=[5];
