1cb5e0a190a0ef162747ff8daf8c40f8647a5cad
oXygen XML Deployer <support@oxygenxml.com>
2018-05-03 09:31:03 +0000

    rebuild pages at 142b582

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,10 @@
+define(function() {var keywords=[{w:"GitHub",p:["p0"]},{w:"DITA-based",p:["p0"]},{w:"Wiki",p:["p0"]},{w:"LW-DITA",p:["p1"]},{w:"GEORGE",p:["p1"]},{w:"at",p:["p1"]},{w:"DITA",p:["p1"]},{w:"NA",p:["p1"]},{w:"Markdown",p:["p2"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6, 7];
+ph["p2"]=[8];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
