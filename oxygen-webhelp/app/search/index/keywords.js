3104e5b65807211ab32b98203485e3848c3adb6c
oXygen XML Deployer <support@oxygenxml.com>
2019-05-07 22:20:59 +0000

    rebuild pages at 562108c

--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,8 +1,8 @@
-define(function() {var keywords=[{w:"GitHub",p:["p0"]},{w:"DITA-based",p:["p0"]},{w:"Wiki",p:["p0"]},{w:"LW-DITA",p:["p1"]},{w:"STC",p:["p1"]},{w:"Markdown",p:["p2"]}];
+define(function() {var keywords=[{w:"GitHub",p:["p0"]},{w:"DITA-based",p:["p0"]},{w:"Wiki",p:["p0"]},{w:"LW-DITA",p:["p1"]},{w:"Markdown",p:["p2"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
-ph["p1"]=[3, 4];
-ph["p2"]=[5];
+ph["p1"]=[3];
+ph["p2"]=[4];
      return {
          keywords: keywords,
          ph: ph
